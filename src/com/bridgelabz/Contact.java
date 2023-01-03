445a04ac5c2d4b390cc66056008f0bbe38f09867
lavans243 <lavans243@gmail.com>
2023-01-03 19:41:01 +0530

    UC6 add multiple

---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,32 @@
+package com.bridgelabz;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class Contact {
+
+    static ArrayList<String > Slot1 = new ArrayList<>();
+    static ArrayList<String > Slot2 = new ArrayList<>();
+    static ArrayList<String> Slot3 = new ArrayList<>();
+    static ArrayList<String> Slot4 = new ArrayList<>();
+    static ArrayList<String > Slot5 = new ArrayList<>();
+
+
+    /*void One (){
+        this.Slot1 = new ArrayList<>();
+    }
+    void Two (){
+        this.Slot2 = new ArrayList<>();
+    }
+    void Three (){
+        this.Slot3 = new ArrayList<>();
+    }
+    void Five (){
+        this.Slot4 = new ArrayList<>();
+    }
+    void Four (){
+        this.Slot5 = new ArrayList<>();
+    }*/
+
+}
---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,41 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main extends Uniquename {
+
+    public static void main(String[] args) {
+
+
+        int exit = 8;
+
+        while (exit == 8) {
+            System.out.println("Please enter the following details: ");
+            System.out.println("1. Create Addressbook");
+            System.out.println("2. Edit Addressbook");
+            System.out.println("3. Delete Addressbook");
+
+
+            Uniquename ob4 = new Uniquename();
+
+            Scanner sc3 = new Scanner(System.in);
+            int inp = sc3.nextInt();
+
+            if (inp == 1) {
+                ob4.check();
+                ob4.Create();
+            } else if (inp == 2) {
+
+                ob4.EditAddressbook();
+
+            } else if (inp == 3) {
+
+                ob4.DeleteAddressbook();
+            } else {
+                System.out.println("Enter the valid input");
+            }
+
+        }
+    }
+    }
