cc05d795a1566276f80487a308277671870e2f21
lavans243 <lavans243@gmail.com>
2023-01-03 23:43:24 +0530

    Welcome UC9 search person

---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
+public class Contact {
+	 static ArrayList<String > Slot1 = new ArrayList<>();
+	    static ArrayList<String > Slot2 = new ArrayList<>();
+	    static ArrayList<String> Slot3 = new ArrayList<>();
+	    static ArrayList<String> Slot4 = new ArrayList<>();
+	    static ArrayList<String > Slot5 = new ArrayList<>();
+
+}
